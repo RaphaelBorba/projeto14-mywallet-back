36b9d62967bafe222f37cd8c62507c993e83aa17
Raphael Borba <raphaelbsx@gmail.com>
2022-12-04 22:26:35 -0300

    router port

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(recipesRouters)
 app.use(usersRouters)
 
 
+const port = process.env.PORT || 5000
 
-app.listen(5000, () => console.log('Server on 5000:'))
+app.listen(port, () => console.log(`Server on ${port}:`))
 
